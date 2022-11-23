99d150171bbc757ce980fae59a50aa863b44238b
santhoshkpatel <snjtec@gmail.com>
2022-11-23 12:33:03 +0530

    1) spring_soil 23/11/2022

---
+++ spring_soil/src/com/xworkz/soil/bean/Soil.java
@@ -0,0 +1,15 @@
+package com.xworkz.soil.bean;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Soil {
+	
+	public Soil() {
+System.out.println("soil running ");
+
+	}
+public void growing() {
+	System.out.println("plants is growing");
+}
+}
